1e5d53b7e53ae6947ae169ee03dd214512a495cb
Valeryaleshka <valeryaleshka@gmail.com>
2020-10-22 17:45:39 +0300

    fix: main page

---
+++ portfoliopages/script.js
@@ -0,0 +1,11 @@
+document.querySelector(".webdev").onclick = function(){
+    window.location.href = "https://valeryaleshka.github.io/Portfolio/portfoliopages/webdev/";
+}
+
+document.querySelector(".calculator").onclick = function(){
+    window.location.href = "https://valeryaleshka.github.io/Portfolio/portfoliopages/calculator/";
+}
+
+document.querySelector(".shelter").onclick = function(){
+    window.location.href = "https://valeryaleshka.github.io/Portfolio/portfoliopages/shelter/pages/main/";
+}
