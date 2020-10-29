621e7dc4d56984b882d643bb1b12148548f36cc6
Valeryaleshka <valeryaleshka@gmail.com>
2020-10-29 12:27:15 +0300

    add: momentum

--- portfoliopages/script.js
+++ portfoliopages/script.js
@@ -7,5 +7,11 @@ document.querySelector(".calculator").onclick = function(){
 }
 
 document.querySelector(".shelter").onclick = function(){
-    window.location.href = "https://valeryaleshka.github.io/Portfolio/portfoliopages/shelter/pages/main/";
+    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/shelter/pages/main/index.html";
 }
+
+document.querySelector(".momentum").onclick = function(){
+    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/momentum/";
+}
+
+
