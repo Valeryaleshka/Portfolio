fde9fdeac252a530d76ccf27b1e6c9e33c98281a
Valeryaleshka <valeryaleshka@gmail.com>
2020-12-14 12:42:49 +0300

    add: new projects

--- portfoliopages/script.js
+++ portfoliopages/script.js
@@ -1,19 +1,16 @@
-document.querySelector(".webdev").onclick = function(){
-    window.location.href = "https://valeryaleshka.github.io//valeryaleshka-JS2020Q3/webdev/";
-}
 
-document.querySelector(".calculator").onclick = function(){
-    window.location.href = "https://valeryaleshka.github.io//valeryaleshka-JS2020Q3/calculator/";
-}
-
-document.querySelector(".shelter").onclick = function(){
-    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/shelter/pages/main/index.html";
-}
-
-document.querySelector(".momentum").onclick = function(){
-    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/momentum/";
-}
 
 document.querySelector(".keyboard").onclick = function(){
     window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/virtual-keyboard/";
 }
+
+
+document.querySelector(".puzzle").onclick = function(){
+    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/gem-puzzle/";
+}
+
+
+document.querySelector(".english").onclick = function(){
+    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/english-for-kids/";
+}
+
