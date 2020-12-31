86b444b02cba75479c4fa8314ef9cf61598eea64
Valeryaleshka <53650916+Valeryaleshka@users.noreply.github.com>
2020-12-31 12:02:23 +0300

    fix: projects

--- portfoliopages/script.js
+++ portfoliopages/script.js
@@ -1,16 +1,12 @@
+document.querySelector(".keyboard").onclick = function () {
+  window.location.href = "https://covid-git-master.esoshyki.vercel.app/about";
+};
 
+document.querySelector(".puzzle").onclick = function () {
+  window.location.href = "https://notes-app.valeryaleshka.vercel.app/";
+};
 
-document.querySelector(".keyboard").onclick = function(){
-    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/virtual-keyboard/";
-}
-
-
-document.querySelector(".puzzle").onclick = function(){
-    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/gem-puzzle/";
-}
-
-
-document.querySelector(".english").onclick = function(){
-    window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/english-for-kids/";
-}
-
+document.querySelector(".english").onclick = function () {
+  window.location.href =
+    "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/english-for-kids/";
+};
