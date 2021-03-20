225fd0e62377285d6b04d19269b77abc72d99b20
Valeryaleshka <53650916+Valeryaleshka@users.noreply.github.com>
2021-03-20 22:13:54 +0300

    fix: add-some-feat

--- portfoliopages/script.js
+++ portfoliopages/script.js
@@ -1,5 +1,5 @@
 document.querySelector(".keyboard").onclick = function () {
-  window.location.href = "https://covid-git-master.esoshyki.vercel.app/about";
+  window.location.href = "https://covid-git-master.esoshyki.vercel.app";
 };
 
 document.querySelector(".puzzle").onclick = function () {
@@ -9,4 +9,19 @@ document.querySelector(".puzzle").onclick = function () {
 document.querySelector(".english").onclick = function () {
   window.location.href =
     "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/english-for-kids/";
+};
+
+document.querySelector(".virtual-piano").onclick = function () {
+  window.location.href =
+    "https://rolling-scopes-school.github.io/valeryaleshka-JSFE2021Q1/virtual-piano/";
+};
+
+document.querySelector(".virtual-keyboard").onclick = function () {
+  window.location.href =
+    "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/virtual-keyboard/";
+};
+
+document.querySelector(".shelter").onclick = function () {
+  window.location.href =
+    "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/virtual-keyboard/";
 };
