af6977f15242224c0110a94fd7cb85ed7780ddfd
Valeryaleshka <53650916+Valeryaleshka@users.noreply.github.com>
2021-06-01 17:28:41 +0300

    add battlecity

--- portfoliopages/script.js
+++ portfoliopages/script.js
@@ -6,22 +6,18 @@ document.querySelector(".puzzle").onclick = function () {
   window.location.href = "https://notes-app.valeryaleshka.vercel.app/";
 };
 
-document.querySelector(".english").onclick = function () {
-  window.location.href =
-    "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/english-for-kids/";
+document.querySelector(".battleCity").onclick = function () {
+  window.location.href = "https://valeryaleshka.github.io/BattleCityIssoft/";
 };
 
 document.querySelector(".virtual-piano").onclick = function () {
-  window.location.href =
-    "https://rolling-scopes-school.github.io/valeryaleshka-JSFE2021Q1/virtual-piano/";
+  window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JSFE2021Q1/virtual-piano/";
 };
 
 document.querySelector(".virtual-keyboard").onclick = function () {
-  window.location.href =
-    "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/virtual-keyboard/";
+  window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/virtual-keyboard/";
 };
 
 document.querySelector(".shelter").onclick = function () {
-  window.location.href =
-    "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/virtual-keyboard/";
+  window.location.href = "https://rolling-scopes-school.github.io/valeryaleshka-JS2020Q3/virtual-keyboard/";
 };
